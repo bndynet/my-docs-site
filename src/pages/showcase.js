596e86693c411c51b7b9ca0a9995c1dba18010fb
Bendy Zhang <zb@bndy.net>
2024-03-29 15:05:52 +0800

    fix: showcase page can not be built

--- src/pages/showcase.js
+++ src/pages/showcase.js
@@ -20,38 +20,41 @@ function getTabs(theme) {
 
 export default function Showcase() {
   const { siteConfig } = useDocusaurusContext();
-  const [items, setItems] = useState(getTabs(window.getCurrentTheme()));
+  const [items, setItems] = useState([]);
   const onThemeChange = (theme) => {
     setItems(getTabs(theme));
   };
 
   let themeChangeId;
   useEffect(() => {
+    setItems(getTabs(window.getCurrentTheme()));
+    themeChangeId = setTimeout(() => {
+      window.listenThemeChange(onThemeChange, {
+        once: true
+      });
+    }, 100);
     return () => {
       if (themeChangeId) {
         clearTimeout(themeChangeId);
       }
     }
-  })
-  themeChangeId = setTimeout(() => {
-    window.listenThemeChange(onThemeChange, {
-      once: true
-    });
-  }, 100);
+  }, []);
   return (
     <Layout
       title={`${siteConfig.title}`}
       description={`${siteConfig.tagline}`}
       noFooter
     >
-      <Tabs className="showcase">
-        {items.map((item, idx) => (
-          <TabItem key={idx} value={item.title} label={item.title} default={idx === 0}>
-            {item.url ? <iframe style={{ width: '100%', height: 'calc(100vh - 129px)' }} scrolling="no" title="Hello World" src={item.url} frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen>
-            </iframe> : <div className='text-center place-content-center text-8xl' style={{ height: 'calc(100vh - 129px)' }}>Coming soon...</div>}
-          </TabItem>
-        ))}
-      </Tabs>
+      {items && items.length > 0 ? (
+        <Tabs className="showcase">
+          {items.map((item, idx) => (
+            <TabItem key={idx} value={item.title} label={item.title} default={idx === 0}>
+              {item.url ? <iframe style={{ width: '100%', height: 'calc(100vh - 129px)' }} scrolling="no" title="Hello World" src={item.url} frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen>
+              </iframe> : <div className='text-center place-content-center text-8xl' style={{ height: 'calc(100vh - 129px)' }}>Coming soon...</div>}
+            </TabItem>
+          ))}
+        </Tabs>
+      ) : <div className='text-center place-content-center text-8xl' style={{ height: 'calc(100vh - 129px)' }}>No Content</div>}
 
     </Layout>
   );
