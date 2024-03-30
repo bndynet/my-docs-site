675e13fef20561a778daf9639e81e6a7224ad802
Bendy Zhang <zb@bndy.net>
2024-03-30 15:11:15 +0800

    fix: theme change does not fire in showcase page

--- src/pages/showcase.js
+++ src/pages/showcase.js
@@ -21,7 +21,9 @@ function getTabs(theme) {
 export default function Showcase() {
   const { siteConfig } = useDocusaurusContext();
   const [items, setItems] = useState([]);
+  const [theme, setTheme] = useState('');
   const onThemeChange = (theme) => {
+    setTheme(theme);
     setItems(getTabs(theme));
   };
 
@@ -38,7 +40,7 @@ export default function Showcase() {
         clearTimeout(themeChangeId);
       }
     }
-  }, []);
+  }, [theme]);
   return (
     <Layout
       title={`${siteConfig.title}`}
@@ -46,7 +48,7 @@ export default function Showcase() {
       noFooter
     >
       {items && items.length > 0 ? (
-        <Tabs className="showcase">
+        <Tabs key={theme} className="showcase">
           {items.map((item, idx) => (
             <TabItem key={idx} value={item.title} label={item.title} default={idx === 0}>
               {item.url ? <iframe style={{ width: '100%', height: 'calc(100vh - 129px)' }} scrolling="no" title="Hello World" src={item.url} frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen>
